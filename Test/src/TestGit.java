d29edb7df3ffc76ae3d2e0aa33c05f0a57696680
supler <neyeng@navercorp.com>
2016-03-30 14:08:01 +0800

    commit second

--- Test/src/TestGit.java
+++ Test/src/TestGit.java
@@ -3,8 +3,9 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("add to more sentence!");
-  System.out.println("add to one more sentence!");
+ System.out.println("add to one more sentence!");
+  System.out.println("add to second more sentence!");
+   System.out.println("add to three more sentence!");
 	}
 
 }
